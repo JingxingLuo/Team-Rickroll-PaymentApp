17f3c88667df82206385fcac614d6c1e167e0130
JingxingLuo <jluo17@mail.sfsu.edu>
2021-12-07 23:33:38 -0800

    doing the error message in easier way

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -7,6 +7,8 @@ const Login = () => {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [redirect, setRedirect] = React.useState(false);
+    const [red,setRed] = React.useState(false);
+    const [messages,setMessages] = React.useState('');
     var storage = window.localStorage;
     var storage2 = window.sessionStorage;
 
@@ -29,6 +31,11 @@ const Login = () => {
                     storage2.setItem("isSuccess", JSON.stringify(data.isSuccess));
                     storage2.setItem("username", JSON.stringify(data.username));
                     storage2.setItem("amount", JSON.stringify(data.amount));
+                    setRed(false);
+                }
+                else{
+                    setRed(true);
+                    setMessages("Invalid username/password!");
                 }
             }).catch(console.log);
     };
@@ -49,6 +56,11 @@ const Login = () => {
                     <h2 className="title">Password</h2>
                     <input type="password" id="inputPassword" className="form-control" placeholder="" required
                         value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                    <div className = "" style = {{paddingTop: "5px"}}>
+                        <div>{red ? <div style = {{color: "red"}}>{messages}</div> 
+                        : <div style = {{color: "green"}}>{messages}</div> }</div>
+                    </div>    
 
                     <button className="submitButtonlog" type="button" onClick={handleSubmit}>Submit</button>
                 </form>
